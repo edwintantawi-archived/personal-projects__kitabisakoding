278c4e049074fc19b952f5173d7b080d565c9dc4
wintantawi <edwintantawi@gmail.com>
2020-08-09 13:19:36 +0700

    On Build | Using BootStrap

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,16 +1,1 @@
 import './component/banner-head.js';
-import './jQuery.js';
-
-var pull = $('#pull'); // Variabel tombol navigasi (akan muncul hanya pada perangkat mobile)
-var menu = $('header nav ul'); // Variabel menu
-$(pull).on('click', function (e) {
-  e.preventDefault();
-  menu.slideToggle();
-
-});
-$(window).resize(function () {
-  var w = $(window).width();
-  if (w > 600 && menu.is(':hidden')) {
-    menu.removeAttr('style');
-  }
-});
