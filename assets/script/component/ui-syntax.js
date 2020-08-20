2986d0555efcb1c67b9911cc0f5d296d6ae2c47a
wintantawi <edwintantawi@gmail.com>
2020-08-20 21:19:01 +0700

    WIP | add more content

--- assets/script/component/ui-syntax.js
+++ assets/script/component/ui-syntax.js
@@ -7,17 +7,19 @@ class UiSyntax extends HTMLElement {
   render() {
 
     this.innerHTML = `
-    <div class="container-fluid d-flex justify-content-center">
-            <div class="row frame">
-              <div class="">
-                <iframe
-                  src="${this.src}"
-                  style="width: 1024px; height: 332px; border:0; transform: scale(1); overflow:scroll;"
-                  sandbox="allow-scripts allow-same-origin">
-                </iframe>
-              </div>
-            </div>
-          </div>
+    <div class="container">
+    <div class="row">
+    <div class="frame">
+    
+    <iframe
+    src="${this.src}"
+    style=" height: 332px; border:0; transform: scale(1); overflow:scroll;"
+    sandbox="allow-scripts allow-same-origin">
+    </iframe>
+    </div>
+
+      </div>
+    </div>
     `
   };
 }
