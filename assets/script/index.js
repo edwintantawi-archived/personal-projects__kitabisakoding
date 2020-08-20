1c6e956d3cac4ddaf94cb573a51df6c56a319878
wintantawi <edwintantawi@gmail.com>
2020-08-20 12:40:03 +0700

    WIP | add website class

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,5 +4,34 @@ import './component/ui-footer.js';
 import './component/banner-info.js';
 import './component/ui-syntax.js';
 
+let user = false;
 
-// hide/show daftar/masuk/user profile
+let accountMode = false;
+
+const userNav = document.querySelector('#userNav');
+const gateway = document.querySelector('#gateway');
+const dividerTwo = document.querySelector('.dividerTwo');
+
+
+const userStatus = user => {
+  if (user) {
+    userNav.classList.add('d-block');
+  } else {
+    gateway.classList.add('d-flex');
+    dividerTwo.classList.add('d-none');
+  }
+}
+
+if (accountMode == false) {
+  userNav.classList.add('d-none');
+  gateway.classList.add('d-none');
+  dividerTwo.classList.add('d-none');
+
+} else {
+  userStatus(user);
+}
+
+export {
+  user,
+  accountMode
+};
--- assets/script/script-index.js
+++ assets/script/script-index.js
@@ -1,30 +1,22 @@
 import './component/ui-navbar.js';
+
 let user = false;
 
 let accountMode = false;
 
-const userNav = document.querySelector('#userNav');
-const gateway = document.querySelector('#gateway');
-const dividerTwo = document.querySelector('.dividerTwo');
 const bannerRegisterButton = document.querySelector('a.btn.btn-warning');
 const bannerAds = document.querySelector('#ads');
 
 const userStatus = user => {
   if (user) {
-    userNav.classList.add('d-block');
     bannerRegisterButton.classList.add('d-none');
     bannerAds.classList.add('d-none');
   } else {
-    gateway.classList.add('d-flex');
-    dividerTwo.classList.add('d-none');
+
   }
 }
 
 if (accountMode == false) {
-  userNav.classList.add('d-none');
-  gateway.classList.add('d-none');
-  dividerTwo.classList.add('d-none');
-
   bannerRegisterButton.classList.add('d-none');
   bannerAds.classList.add('d-none');
 } else {
